0eaf43613861c3dac12abab50a044a91c4d321ff
Oleksii Svitiashchuk <svityashchuk.oleksiy@gmail.com>
2023-07-02 17:46:50 +0200

    Upcoming leaves - simple list and API endpoint

---
+++ sqlite/leave.go
@@ -0,0 +1,59 @@
+package sqlite
+
+import (
+	"database/sql"
+	"ptocker"
+	"time"
+)
+
+type LeaveService struct {
+	db *sql.DB
+}
+
+// TODO pass DB
+func NewLeaveService() (*LeaveService, error) {
+	db, err := sql.Open("sqlite3", DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LeaveService{db}, nil
+}
+
+func (ls *LeaveService) List(from, to time.Time, limit int) ([]*ptocker.Leave, error) {
+	rows, err := ls.db.Query(`
+		SELECT
+		l.id, l.user_id, l.start, l.end, l.type, l.approved
+		FROM leaves l
+		INNER JOIN users u ON l.user_id = u.id
+		WHERE l.start >= ? and l.end <= ?
+	`,
+		from.Format("2006-01-02 15:04:05"),
+		to.Format("2006-01-02 15:04:05"),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ll := []*ptocker.Leave{}
+	for rows.Next() {
+		leave := ptocker.Leave{}
+		err = rows.Scan(
+			&leave.ID,
+			&leave.UserID,
+			&leave.Start,
+			&leave.End,
+			&leave.Type,
+			&leave.Approved,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		ll = append(ll, &leave)
+	}
+
+	return ll, nil
+}
