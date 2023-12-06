0b57f4a89a633a60bf44806ebebb491268278796
Oleksii Svitiashchuk <svitiashchuk.oleksiy@gmail.com>
2023-12-06 23:11:41 +0100

    WIP simple middleware and simple session manager

---
+++ http/session.go
@@ -0,0 +1,50 @@
+package http
+
+import "errors"
+
+type SessionManager interface {
+	get(id string) *Session
+	create(id string) (*Session, error)
+}
+
+type SessionKeeper struct {
+	sessions map[string]*Session
+}
+
+func NewSessionKeeper() SessionManager {
+	return &SessionKeeper{
+		sessions: make(map[string]*Session),
+	}
+}
+
+func (sk *SessionKeeper) get(id string) *Session {
+	return sk.sessions[id]
+}
+
+func (sk *SessionKeeper) create(id string) (*Session, error) {
+	if _, exists := sk.sessions[id]; exists {
+		return nil, errors.New("session already exists")
+	}
+
+	sk.sessions[id] = &Session{
+		vals: make(map[string]string),
+	}
+
+	return sk.sessions[id], nil
+}
+
+type Session struct {
+	vals map[string]string
+}
+
+func get(k string) error {
+	return errors.New("not implemented")
+}
+
+func set(k string, v string) error {
+	return errors.New("not implemented")
+}
+
+func del(k string) error {
+	return errors.New("not implemented")
+}
