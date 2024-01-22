20d5a6144324a23f0d4efceba154d2344261f12e
Oleksii Svitiashchuk <svityashchuk.oleksiy@gmail.com>
2024-01-22 19:48:03 +0100

    Move date-related code to calendar.go

--- cmd/ssr/app.go
+++ cmd/ssr/app.go
@@ -14,11 +14,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type MonthPeriod struct {
-	Month time.Month
-	Year  int
-}
-
 type Navigation struct {
 	Prev MonthPeriod
 	Now  MonthPeriod
@@ -215,60 +210,6 @@ func (app *App) handleLeaveReject(w http.ResponseWriter, r *http.Request) {
 
 func (app *App) htmxRedirect(w http.ResponseWriter, r *http.Request, url string) {
 	w.Header().Add("HX-Redirect", url)
-}
-
-func (mp MonthPeriod) MonthNum() int {
-	return int(mp.Month)
-}
-
-func month(year, month int) []time.Time {
-	days := []time.Time{}
-	initial := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
-
-	for i := 0; i < daysInMonth(year, month); i++ {
-		days = append(days, initial.AddDate(0, 0, i))
-	}
-
-	return days
-}
-
-func daysInMonth(year, month int) int {
-	d := time.Date(year, time.Month(month+1), 0, 0, 0, 0, 0, time.UTC)
-
-	return d.Day()
-}
-
-func period(start, end time.Time) []time.Time {
-	days := []time.Time{}
-	for d := start; d.Before(end); d = d.AddDate(0, 0, 1) {
-		days = append(days, d)
-	}
-
-	return days
-}
-
-func days(year int) []time.Time {
-	days := []time.Time{}
-	initial := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
-	time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
-
-	for i := 0; i < daysInYear(year); i++ {
-		days = append(days, initial.AddDate(0, 0, i))
-	}
-
-	return days
-}
-
-func daysInYear(year int) int {
-	if isLeap(year) {
-		return 366
-	}
-
-	return 365
-}
-
-func isLeap(year int) bool {
-	return year%4 == 0 && year%100 != 0 || year%400 == 0
 }
 
 func (app *App) commonTemplateData(r *http.Request) *CommonTemplateData {
---
+++ cmd/ssr/calendar.go
@@ -0,0 +1,62 @@
+package main
+
+import "time"
+
+type MonthPeriod struct {
+	Month time.Month
+	Year  int
+}
+
+func (mp MonthPeriod) MonthNum() int {
+	return int(mp.Month)
+}
+
+func month(year, month int) []time.Time {
+	days := []time.Time{}
+	initial := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+
+	for i := 0; i < daysInMonth(year, month); i++ {
+		days = append(days, initial.AddDate(0, 0, i))
+	}
+
+	return days
+}
+
+func daysInMonth(year, month int) int {
+	d := time.Date(year, time.Month(month+1), 0, 0, 0, 0, 0, time.UTC)
+
+	return d.Day()
+}
+
+func days(year int) []time.Time {
+	days := []time.Time{}
+	initial := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	for i := 0; i < daysInYear(year); i++ {
+		days = append(days, initial.AddDate(0, 0, i))
+	}
+
+	return days
+}
+
+func daysInYear(year int) int {
+	if isLeap(year) {
+		return 366
+	}
+
+	return 365
+}
+
+func isLeap(year int) bool {
+	return year%4 == 0 && year%100 != 0 || year%400 == 0
+}
+
+func period(start, end time.Time) []time.Time {
+	days := []time.Time{}
+	for d := start; d.Before(end); d = d.AddDate(0, 0, 1) {
+		days = append(days, d)
+	}
+
+	return days
+}
